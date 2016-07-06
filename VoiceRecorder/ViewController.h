dc57870a1c25f31a79040edee96ea2160509880b
Bibinur <zhursinbek.b@nisa.edu.kz>
2016-07-06 11:57:09 -0400

    Initial Commit

---
+++ VoiceRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VoiceRecorder
+//
+//  Created by Bibinur on 7/6/16.
+//  Copyright © 2016 Bibinur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
